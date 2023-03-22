a63c49fc77ebcbc6df5d2c9fbd8c161dde31b9af
raineer <delaritaraineer81@gmail.com>
2023-03-23 04:30:30 +0800

    vercel

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { map,tap } from 'rxjs/operators';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  constructor(private authService: AuthenticationService) {}
-
-  ngOnInit(): void {}
+export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  })
+  constructor(
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
   login() {
-    console.log('test');
-    console.log('login');
-    return this.authService.login('raineerdelarita81@gmail.com', 'test123').subscribe((data) => console.log('success'));
+    if (this.form.valid) {
+      this.authService.login({
+        email: this.email.value,
+        password: this.password.value
+      }).pipe(
+        tap(() => this.router.navigate(['register']))
+      ).subscribe()
+    }
   }
+
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
+
+
+ 
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, tap, switchMap } from 'rxjs/operators';
+
+export interface LoginForm {
+  email: string;
+  password: string;
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string) {
-    return this.http.post<any>('/api/auth/signin', { email, password }).pipe(
+  login(loginForm: LoginForm) {  
+
+    return this.http.post<any>('/api/auth/signin', {email: loginForm.email, password: loginForm.password}).pipe(
       map((token) => {
-        console.log('token', token);
-        console.log('tooken');
+      
+        return token;
       })
-    );
+    )
   }
 }
