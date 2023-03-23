974d3184e67e5450f00bb90ce8697e7ca023c967
raineer <delaritaraineer81@gmail.com>
2023-03-23 19:12:48 +0800

    register service

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, tap, switchMap } from 'rxjs/operators';
+import { map, tap, switchMap, catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserI } from '../model/user.interface';
 export interface LoginForm {
   email: string;
   password: string;
@@ -11,7 +13,7 @@ export interface LoginForm {
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private snackbar: MatSnackBar) {}
   appRoot = environment.appRoot;
   login(loginForm: LoginForm) {
     console.log(this.appRoot);
@@ -26,5 +28,33 @@ export class AuthenticationService {
           return token;
         })
       );
+  }
+
+  create(user: UserI): Observable<UserI> {
+    return this.http.post<UserI>(`${this.appRoot}/api/auth/register`, user).pipe(
+      tap((createdUser: UserI) =>
+        this.snackbar.open(
+          `User ${createdUser.email} created successfully`,
+          'Close',
+          {
+            duration: 2000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+          }
+        )
+      ),
+      catchError((e) => {
+        this.snackbar.open(
+          `User could not be created, due to: ${e.error.message}`,
+          'Close',
+          {
+            duration: 5000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+          }
+        );
+        return throwError(e);
+      })
+    );
   }
 }
