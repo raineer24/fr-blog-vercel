888d504f24ab1ef4ab4b4f3b64201d945c87d0ab
raineer <delaritaraineer81@gmail.com>
2023-03-26 21:27:29 +0800

    app component change ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fr-blog02';
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  navigateTo(value: any) {
+    this.router.navigate(['../', value]);
+    console.log('router', this.router.navigate(['/public/', value]));
+  }
 }
