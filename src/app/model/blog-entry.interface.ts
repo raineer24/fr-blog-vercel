00f2665484f22ef233b46445b0193d4413ff0080
raineer <delaritaraineer81@gmail.com>
2024-01-02 14:58:01 +0800

    content ?

--- src/app/model/blog-entry.interface.ts
+++ src/app/model/blog-entry.interface.ts
@@ -1,16 +1,15 @@
-import { UserI } from "./user.interface";
+import { UserI } from './user.interface';
 
 export interface BlogEntry {
-    id?: number;
-    title?: string;
-    slug?: string;
-    description?: string;
-    body?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-    likes?: number;
-    author?: UserI;
-    headerImage?: string;
-    publishedDate?: Date;
-    isPublished?: boolean;
+  id?: number;
+  title?: string;
+  slug?: string;
+  content?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  likes?: number;
+  author?: UserI;
+  headerImage?: string;
+  publishedDate?: Date;
+  isPublished?: boolean;
 }
