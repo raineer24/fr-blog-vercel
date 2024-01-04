93400cb86cb71e7fbeff2c82bf6cf3e06b90e8fb
raineer <delaritaraineer81@gmail.com>
2024-01-04 16:43:41 +0800

    blog services

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -4,7 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { AuthenticationService } from './authentication.service';
-import { BlogEntry } from '../model/blog-entry.interface';
+import { BlogEntriesPageable, BlogEntry } from '../model/blog-entry.interface';
 
 export interface UserData {
     results: BlogEntry[];
@@ -16,4 +16,36 @@ export interface UserData {
       startIndex: number;
       endIndex: number;
     };
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class BlogService {
+    appRoot = environment.appRoot;
+    constructor(private http: HttpClient) { }
+
+    findOne(id: number): Observable<BlogEntry> {
+      const url = `${this.appRoot}/api/blogs/`;
+      return this.http.get<BlogEntry>(url + id);
+    }
+  
+    indexAll(page: number, limit: number): Observable<BlogEntriesPageable> {
+      let params = new HttpParams();
+  
+      params = params.append('page', String(page));
+      params = params.append('limit', String(limit));
+  
+      return this.http.get<BlogEntriesPageable>('/api/blogs/pages?', {params});
+    }
+  
+    indexByUser(userId: number, page: number, limit: number): Observable<BlogEntriesPageable> {
+      let params = new HttpParams();
+  
+      params = params.append('page', String(page));
+      params = params.append('limit', String(limit));
+  
+      return this.http.get<BlogEntriesPageable>('/api/blog-entries/user/' + String(userId), {params});
+    }
+  
   }
